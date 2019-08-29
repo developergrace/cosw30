8a0ebcf61f1e45925765d1d780e7c257df13d701
EC2 Default User <ec2-user@ip-172-31-84-105.ec2.internal>
2019-08-29 02:58:30 +0000

    created lab 1 file and added ipsum

---
+++ labwork/lab1.php
@@ -0,0 +1,16 @@
+<!doctype HTML>
+<html>
+    <head>
+        <title>My First PHP Page</title>
+    </head>
+    
+    <body>
+        <h1>Grace Park</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Nam libero justo laoreet sit amet cursus sit. Donec massa sapien faucibus et molestie ac feugiat sed lectus. Eu non diam phasellus vestibulum. Ullamcorper sit amet risus nullam. Lacus luctus accumsan tortor posuere ac ut consequat. Cras tincidunt lobortis feugiat vivamus.</p>
+        
+        <?php
+            echo 'This is my beautiful PHP paragraph.';
+        ?>
+        
+    </body>
+</html>
