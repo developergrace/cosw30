852169b34aeb7b2dafcdf5dc59d839667f8bd780
EC2 Default User <ec2-user@ip-172-31-84-105.ec2.internal>
2019-09-01 04:09:06 +0000

    Added p tags inside PHP

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -24,8 +24,10 @@
             While you're here, check out my <a href="https://github.com/developergrace" target="_blank">Github account</a>.
         </p>
         
-        <?php echo 'As for websites, I like to watch food-related videos on <a href="https://www.youtube.com/" target=_blank">Youtube</a> and browse <a ref="https://www.reddit.com/" target="_blank">Reddit</a> for interesting or humorous stories.';
-        echo 'This was my first time using PHP so it was a little hard for me to wrap my head around and get used to. In particular, I had trouble with escaping the quotation marks inside the a tags in the previous paragraph. Although typing PHP can be hard on the eyes, after reading Chapter 1 of the textbook, I now understand the benefits of using PHP and look forward to learning more.';
+        <?php echo
+            '<p>As for websites, I like to watch food-related videos on <a href="https://www.youtube.com/" target=_blank">Youtube</a> and browse <a href="https://www.reddit.com/" target="_blank">Reddit</a> for interesting or humorous stories.</p>
+            
+            <p>This was my first time using PHP so it was a little hard for me to wrap my head around and get used to. In particular, I had trouble with escaping the quotation marks inside the a tags in the previous paragraph. Although typing PHP can be hard on the eyes, after reading Chapter 1 of the textbook, I now understand the benefits of using PHP and look forward to learning more.</p>';
         ?>
         
     </body>
