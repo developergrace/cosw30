96edde7f0384356922894d07dfef231a6f4983e8
EC2 Default User <ec2-user@ip-172-31-84-105.ec2.internal>
2019-09-01 04:34:41 +0000

    Added CSS

--- labwork/lab1.php
+++ labwork/lab1.php
@@ -2,6 +2,31 @@
 <html>
     <head>
         <title>Labwork 1: Hello World</title>
+        <style>
+            body {
+                background-image: url('http://thepatternlibrary.com/img/r.png');
+                margin: 0;
+                font-family: Verdana, Geneva, sans-serif;
+            }
+            
+            main {
+                background-color: rgba(255,255,255,.9);
+                border-radius: 10px;
+                margin: 1% 10%;
+                padding: 1% 5%;
+            }
+            
+            a {
+                text-decoration: none;
+                color: #E3655B;
+                font-weight: bold;
+            }
+            
+            a:hover {
+                color: #129490;
+            }
+
+        </style>
     </head>
     
     <body>
@@ -13,22 +38,23 @@
         */
         ?>
         
-        <h1>Welcome to my First PHP Page!</h1>
-        <p>
-            My name is Grace and I am a student at LBCC. I am pursuing a career in web development. I have no experience with backend languages so PHP is entirely new to me. This is also my first semester back as a full-time student so I am a little worried about getting adjusted.
-        </p>
-        <p>
-            My hobbies include video games, UFC, and food. I like fiction novels of the mystery, science fiction, domestic, and absurdist varities. I would greatly appreciate any audiobook recommendations!   
-        </p>
-        <p>
-            While you're here, check out my <a href="https://github.com/developergrace" target="_blank">Github account</a>.
-        </p>
+        <main>
+            <h1>Welcome to my First PHP Page!</h1>
+            <p>
+                My name is Grace and I am a student at LBCC. I am pursuing a career in web development. I have no experience with backend languages so PHP is entirely new to me. This is also my first semester back as a full-time student so I am a little worried about getting adjusted.
+            </p>
+            <p>
+                My hobbies include video games, UFC, and food. I like fiction novels of the mystery, science fiction, domestic, and absurdist varities. I would greatly appreciate any audiobook recommendations!   
+            </p>
+            <p>
+                While you're here, check out my <a href="https://github.com/developergrace" target="_blank">Github account</a>.
+            </p>
         
-        <?php echo
-            '<p>As for websites, I like to watch food-related videos on <a href="https://www.youtube.com/" target=_blank">Youtube</a> and browse <a href="https://www.reddit.com/" target="_blank">Reddit</a> for interesting or humorous stories.</p>
+            <?php echo
+                '<p>As for websites, I like to watch food-related videos on <a href="https://www.youtube.com/" target=_blank">Youtube</a> and browse <a href="https://www.reddit.com/" target="_blank">Reddit</a> for interesting or humorous stories.</p>
             
-            <p>This was my first time using PHP so it was a little hard for me to wrap my head around and get used to. In particular, I had trouble with escaping the quotation marks inside the a tags in the previous paragraph. Although typing PHP can be hard on the eyes, after reading Chapter 1 of the textbook, I now understand the benefits of using PHP and look forward to learning more.</p>';
-        ?>
-        
+                <p>This was my first time using PHP so it was a little hard for me to wrap my head around and get used to. In particular, I had trouble with escaping the quotation marks inside the a tags in the previous paragraph. Although typing PHP can be hard on the eyes, after reading Chapter 1 of the textbook, I now understand the benefits of using PHP and look forward to learning more.</p>';
+            ?>
+        </main>
     </body>
 </html>
