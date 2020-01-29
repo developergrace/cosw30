e6631bf76cd520787f6a0e670ea7dc85185e1989
developergrace <gpark2351@gmail.com>
2020-01-29 23:10:58 +0000

    updated index.php

---
+++ index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Grace Park | Portfolio</title>
+    <meta name="description" content="front-end web developer development grace park">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.5/css/bulma.min.css">
+    <link rel="stylesheet" href="/includes/styles.scss">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.2.2/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.4/jquery-ui.min.js"></script>
+    <link rel="shortcut icon" type="image/x-icon" href="/images/favicon.ico">
+</head>
+
+<body>
+<div class="columns">
+<header class="column is-12">
+  <nav id="navbar" class="navbar is-danger is-fixed-top" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="#welcome-section">Grace Park</a>
+      <div class="navbar-burger burger" onclick="document.querySelector('.navbar-menu').classList.toggle('is-active');">
+    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      </a>
+    </div>
+  </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-end flex">
+      <a class="navbar-item" href="#projects">Projects</a>
+      <a class="navbar-item drop" href="#about">About</a>
+      <a class="navbar-item" href="#contact">Contact</a>
+      </div>
+    </div>
+</nav>
+</header>
+</div>
+
+<main>  
+  <section id="welcome-section" class="hero parallax">
+    <div class="hero-body">
+      <div class="container">
+        <h1 class="title has-text-centered is-size-1">Hi, I&#39;m Grace</h1>
+        <h2 class="subtitle column has-text-centered">Web Developer in Training</h2>
+        </div></div>
+  </section>
+  
+  <section id="projects">
+    <div class="columns">
+    <h3 class="title column is-offset-1">Projects</h3></div>
+    
+    <div class="columns">
+      
+      <div class="column is-offset-1 is-5">
+<a href="https://cosw30-grace.herokuapp.com/homework/homework10/crud.php" target="_blank"><div class="card project-tile">
+  <div class="card-image">
+    <figure class="image">
+      <img src="/images/project1.PNG" alt="Adventure Awaits">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4 has-text-centered">PHP & mySQL</p>
+      </div></div></div></div></a></div>
+
+      <div class="column is-5">
+<article class="message is-warning">
+  <div class="message-header">
+    <p>PHP & mySQL</p>
+  </div>
+  <div class="message-body">
+    A registration form created using PHP and connected to a mySQL database. Entered information is also automatically generated in a table displayed on the page; don&#39;t share sensitive information!
+    </div></article>
+  </div>
+      
+      
+      </div>
+  
+
+    <div class="columns order">
+<div class="column is-5 is-offset-1">
+<article class="message is-warning">
+  <div class="message-header">
+    <p>HTML & CSS</p>
+  </div>
+  <div class="message-body">
+    A previous version of my portfolio featuring hover effects using pure HTML & CSS. Click around to see some of my older projects!</div></article>
+  </div>
+      
+      <div class="column is-5">
+<a href="/portfolio/index.php" target="_blank"><div class="card project-tile">
+  <div class="card-image">
+    <figure class="image">
+      <img src="/images/oldportfolio.PNG" alt="Portfolio Page">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4 has-text-centered">HTML & CSS</p>
+      </div></div></div></div></a></div></div>
+
+
+    <div class="columns">
+      <div class="column is-5 is-offset-1">
+<a href="https://codepen.io/kgpark/full/ewKoYb" target="_blank"><div class="card project-tile">
+  <div class="card-image">
+    <figure class="image">
+      <img src="/images/project3.PNG" alt="Flexbox">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4 has-text-centered">CSS Flex</p>
+      </div></div></div></div></a></div>
+      
+      <div class="column is-5">
+<article class="message is-warning">
+  <div class="message-header">
+    <p>CSS Flex</p>
+  </div>
+  <div class="message-body">
+    Quick and fun fully responsive (mobile-friendly) page I created using CSS flex in Codepen.
+    </div></article>
+  </div>
+      
+      </div>
+    
+    
+    <div class="columns order">
+        <div class="column is-5 is-offset-1 ">
+            <article class="message is-warning">
+            <div class="message-header">
+                <p>Bootstrap</p>
+            </div>
+            <div class="message-body">
+                Fully responsive page made using the Bootstrap framework in Codepen.</div></article>
+            </div>
+      
+        <div class="column is-5">
+            <a href="https://codepen.io/kgpark/full/bXeeag" target="_blank"><div class="card project-tile">
+            <div class="card-image">
+                <figure class="image">
+                    <img src="/images/bootstrap.PNG" alt="Bootstrap Practice">
+                </figure>
+            </div>
+            <div class="card-content">
+                <div class="media">
+                    <div class="media-content">
+                        <p class="title is-4 has-text-centered">Bootstrap Practice</p>
+                    </div>
+                </div>
+            </div></a>
+        </div>
+    </div>
+</div>
+        <div class="columns">
+      <div class="column is-5 is-offset-1">
+<a href="/homework/homework2/contact.html" target="_blank"><div class="card project-tile">
+  <div class="card-image">
+    <figure class="image">
+      <img src="/images/ferd.PNG" alt="Response Form">
+    </figure>
+  </div>
+  <div class="card-content">
+    <div class="media">
+      <div class="media-content">
+        <p class="title is-4 has-text-centered">PHP Form</p>
+      </div></div></div></div></a></div>
+      
+      <div class="column is-5">
+<article class="message is-warning">
+  <div class="message-header">
+    <p>PHP Form</p>
+  </div>
+  <div class="message-body">
+    Response form created using PHP. Completing the form results in a thank you message confirming your details.
+    </div></article>
+  </div>
+      
+      </div>
+    
+    
+  </section>
+    
+  <div class="mobile">
+  <section id="about" class="parallax">
+    <div class="columns">
+    <h3 class="title column is-offset-1">About</h3></div>
+
+    <div class="columns size">
+    <img src="/images/gp.jpg" alt="Grace Park" class="column is-one-quarter is-offset-1">
+      
+    <p class="column is-6">This is the second class I've taken for the web development certification program at LBCC. There were times I felt overwhelmed with the material but I've learned so much about web development from this course including how to use frameworks and processors, and create responsive websites using flex and grid. My goal is to become a front-end web developer so these are all valuable skills I will take with me. </p></div>
+    
+    <div class="columns">
+    <p class="column is-8 is-offset-1">For this final project, we were instructed to apply two topics from the technical documentation project. I wanted to explore as many topics as I could so I chose topics I didn't write about. The topics I chose are:</p></div>
+  
+  <div class="columns">
+    <p class="column is-8 is-offset-1"><strong>Bulma</strong> &ndash; This page is made using the Bulma framework. I had an easier time understanding Bulma's column-based layout than Bootstrap's row-based layout, mostly due to Bulma's &apos;is-offset-by&apos; class which gives me full grid control.</p></div>
+    
+  <div class="columns">
+    <p class="column is-8 is-offset-1">The downsides of using a framework include limited design, unnecessary code, and being required to use the framework's semantics. I definitely struggled with these downsides while using Bulma for this page, but this helped me understand when and where a framework is most appropriate to use.</p></div>
+  
+   <div class="columns"> 
+    <p class="column is-8 is-offset-1"><strong>Parallax Scrolling</strong> &ndash; I've always liked the parallax scrolling effect for the added visual interest it gives a page so I knew I wanted to use it for my final project. I originally tried a pure CSS approach, however, the resulting page negatively affected the user experience when used in conjunction with the Bulma framework, so I switched to the JS method.
+     </p></div>
+  </section>
+  
+  <section id="contact" class="hero">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <p class="title is-size-2">I'm Free For Lunch</p><a class="subtitle is-size-4" id="profile-link" href="https://www.freecodecamp.org/gracep" target="_blank">See My Profile</a> | <a class="subtitle is-size-4" href="mailto:gpark2351@gmail.com">Email Me</a>
+    </div></div></section>
+  </div>
+</main>
+
+<script>
+$(document).ready(function(){
+  var previousScroll = 0;
+  var d = 0;
+	var s = 0;
+
+  $(window).scroll(function() {
+    var currentScroll = $(this).scrollTop();
+      if (currentScroll > previousScroll){
+          d = (s - .9);
+        $('.parallax').css('background-position-y',d);
+      } else {
+          d = (s + .9);
+        $('.parallax').css('background-position-y',d);
+      }
+      previousScroll = currentScroll;
+      s = d;
+  });
+});
+</script>
+
+</body>
+</html>
--- portfolio/index.php
+++ portfolio/index.php
@@ -8,35 +8,23 @@ Want to create something together? <a href="mailto:gpark2351@gmail.com">Let's ta
 <hr>        
 
 <div class="projects">
-    <img src="images/project1.PNG" alt="CRUD Camp">
+    <img src="images/project1.PNG" alt="Alice in Wonderland">
         <div class="overlay">
             <div class="info">
-                <h3>PHP & SQL</h3>
-                <p>An exercise in creating databases and editable records with PHP and SQL. Don't worry, no sensitive information
-                was breached.</p>
-                <a href="https://cosw30-grace.herokuapp.com/homework/homework10/crud.php" target="_blank">View Site</a>
+                <h3>Alice in Wonderland</h3>
+                <p>An online storybook with selected page tabs and optimized content. Pure HTML & CSS.</p>
+                <a href="/labwork/alice/Alice7.html" target="_blank">View Site</a>
             </div>
         </div>
 </div>
 
 <div class="projects">
-    <img src="images/project2.PNG" alt="Codepen Portfolio">
+    <img src="images/bangkok.PNG" alt="Bangkok Travel Brochure">
         <div class="overlay">
             <div class="info">
-                <h3>Bulma & Parallax</h3>
-                <p>A portfolio created for class in Codepen. Made using the Bulma framework with a touch of Javascript.</p>
-                <a href="https://codepen.io/kgpark/full/xvXOav" target="_blank">View Site</a>
-            </div>
-        </div>
-</div>
-
-<div class="projects">
-    <img src="images/project3.PNG" alt="Flexbox Hotel">
-        <div class="overlay">
-            <div class="info">
-                <h3>Flex</h3>
-                <p>Created entirely using HTML and CSS, made responsive with flex.</p>
-                <a href="https://codepen.io/kgpark/full/ewKoYb" target="_blank">View Site</a>
+                <h3>Travel Blog</h3>
+                <p>2-page blog-style site utilizing forms, tables, and anchored tags.</p>
+                <a href="/labwork/bangkok/Bangkok.html" target="_blank">View Site</a>
             </div>
         </div>
 </div>
@@ -48,6 +36,18 @@ Want to create something together? <a href="mailto:gpark2351@gmail.com">Let's ta
                 <h3>HTML & CSS</h3>
                 <p>One of the first pages I created. A landing page for LBCC's web development program.</p>
                 <a href="https://cosw30-grace.herokuapp.com/labwork/lbcc/index.html" target="_blank">View Site</a>
+            </div>
+        </div>
+        
+</div>
+
+<div class="projects">
+    <img src="images/project2.PNG" alt="Codepen Portfolio">
+        <div class="overlay">
+            <div class="info">
+                <h3>Bulma & Parallax</h3>
+                <p>My new & improved portfolio featuring more recent projects and made using the Bulma framework with a touch of Javascript.</p>
+                <a href="/" target="_blank">View Site</a>
             </div>
         </div>
 </div>
